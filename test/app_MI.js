a3ee80747689506d02f6c09af2ae94bfc904d3f9
renhz <blestmilk@hotmail.com>
2022-02-14 11:31:20 +0800

    將測試檔案獨立到test資料夾

--- test/app_MI.js
+++ test/app_MI.js
@@ -9,7 +9,7 @@ app.listen(3000);
 var getYahoo = require('./twstock.js');
 
 app.use(express.static('./public'))
-app.use(express.static('./node_modules'))
+app.use(express.static('../node_modules'))
 app.use(express.json()); // 解析 JSON 資料
 app.use(express.urlencoded({ extended: true })); //解析 Form Data
 console.log("crawler start...")
