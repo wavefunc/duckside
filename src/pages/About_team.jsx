9733442e16d314ff3d66b1e70fae1ffdd06c1a30
wavefunc <wavefunc@gmail.com>
2022-02-17 13:44:30 +0800

    完成 about_team 外觀

--- src/pages/About_team.jsx
+++ src/pages/About_team.jsx
@@ -1,7 +1,7 @@
 // ----- 冠樺 ----- //
 
 import React, { Component } from 'react';
-import { Row, Col, Container, Card } from 'react-bootstrap';
+import { Row, Col, Container, Card, ListGroup } from 'react-bootstrap';
 
 class About_team extends Component {
    state = {}
@@ -9,14 +9,79 @@ class About_team extends Component {
       return (
          <Container>
             <Row>
-               <Col>1 of 2</Col>
-               <Col>2 of 2</Col>
-               <Col>2 of 2</Col>
-            </Row>
-            <Row>
-               <Col>1 of 3</Col>
-               <Col>2 of 3</Col>
-               <Col>3 of 3</Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon01.png" />
+                     <Card.Body>
+                        <Card.Title>鄭人豪</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title and make up the bulk of
+                           the card's content.
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card xs={12} md={6} lg={4}>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon02.png" />
+                     <Card.Body>
+                        <Card.Title>李巧琳</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title and make up the bulk of
+                           the card's content.
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card xs={12} md={6} lg={4}>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon03.png" />
+                     <Card.Body>
+                        <Card.Title>蔡沛珊</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title and make up the bulk of
+                           the card's content.
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card xs={12} md={6} lg={4}>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon04.png" />
+                     <Card.Body>
+                        <Card.Title>陳鎧洋</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title and make up the bulk of
+                           the card's content.
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card xs={12} md={6} lg={4}>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon05.png" />
+                     <Card.Body>
+                        <Card.Title>李晴暄</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title and make up the bulk of
+                           the card's content.
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col xs={12} md={6} lg={4}>
+                  <Card xs={12} md={6} lg={4}>
+                     <Card.Img variant="top" src="\assets\about_team\pokemon06.jpg" />
+                     <Card.Body>
+                        <Card.Title>李冠樺</Card.Title>
+                        <Card.Text>
+                           建立專案檔案架構<br />
+                           架設資料庫<br />
+                           資料庫銜接前端的程式
+                        </Card.Text>
+                     </Card.Body>
+                  </Card>
+               </Col>
             </Row>
          </Container>
       );
