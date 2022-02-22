6b6928f1bcd6aba6057d8a5802cc8f4908b9fba8
wavefunc <wavefunc@gmail.com>
2022-02-22 13:17:23 +0800

    update

--- backend/routerTransaction.js
+++ backend/routerTransaction.js
@@ -105,14 +105,17 @@ router.post('/transaction/inventory', async (req, res) => {
 });
 
 // ****************************************************
-// 查詢最近交易紀錄，可利用 amount參數 自行設定要幾筆
+// 依日期 DESC 查詢最近交易紀錄，可利用 amount參數 自行設定要查幾筆
+// 若 amount 為 null (即不傳值)，則依日期 ASC 排序，回傳所有交易紀錄
 // ****************************************************
 router.post('/transaction/recent', async (req, res) => {
    // 透由前端傳過來的 acc_email 檢查帳號是否存在，並取得 acc_id
    var acc_id = await checkAccount(req.body.acc_email, res);
+
+   var strLimit = (req.body.amount) ? `DESC LIMIT ${req.body.amount}` : ``;
    
    var strQuery = `SELECT * FROM transaction WHERE acc_id = ?
-   ORDER BY txn_date DESC LIMIT ${req.body.amount}`;
+   ORDER BY txn_date ${strLimit}`;
    
    query(strQuery, [acc_id], (err, rows) => {
       err ? res.send(err) : res.send(rows);
