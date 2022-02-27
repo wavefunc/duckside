3a87f6825ea31fc94ba93c3e2d12234a79bf6326
wavefunc <wavefunc@gmail.com>
2022-02-27 21:48:22 +0800

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
-// ----- 冠樺 ----- //
+// ----- 沛珊 ----- //
 
 import React, { Component } from 'react';
-// import { Link } from "react-router-dom";
 import { Image } from 'react-bootstrap';
 
 class Homepage extends Component {
@@ -10,7 +9,6 @@ class Homepage extends Component {
          <div className="row">
             <div className="col-lg-12">
                <Image src='/assets/images/homepage.jpg' fluid/>
-               {/* <Link to="/backend/datatype">資料庫取得的資料型態</Link><br /> */}
             </div>
          </div>
       );
