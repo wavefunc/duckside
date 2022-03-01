3d2e628c7b2a2e5421cdbd5ccf1ef5c91ea52d0b
wavefunc <wavefunc@gmail.com>
2022-03-01 17:17:25 +0800

    update

--- src/components/BackendAvatar.jsx
+++ src/components/BackendAvatar.jsx
@@ -8,6 +8,7 @@ var fileReader = new FileReader();
 
 function BackendAvatar() {
    const [avatar, setAvatar] = useState('');
+   const [upToDate, setUpToDate] = useState(true);
 
    useEffect(() => {
       axios.post('http://localhost:5000/account/list',
@@ -16,8 +17,46 @@ function BackendAvatar() {
       )
          .then(res => {
             setAvatar(res.data.acc_avatar);
+            setUpToDate(true);
          });
-   }, []);
+   }, [upToDate]);
+
+   function uploadAvatar(e) {
+      // 檔案位置
+      // console.log(e.target.files[0]);
+
+      // 新增 formData
+      const formData = new FormData();
+      e.target.files[0].acc_email = 4;
+      formData.append('image', e.target.files[0]);
+      formData.append('acc_email', 'ggg@mail.com');
+      // console.log('hello' + formData.get('acc_email'));
+
+      // 傳送資料
+      // axios.put('http://localhost:5000/account/updateavatar', formData, {
+      //    headers: {
+      //       'Content-Type': 'multipart/form-data',
+      //    }
+      // }).then(res => {
+      //    console.log(res.data);
+      //    setUpToDate(false);
+      // });
+
+      axios({
+         method: 'put',
+         url: 'http://localhost:5000/account/updateavatar',
+         data: formData,
+         headers: {
+            'Content-Type': 'multipart/form-data'
+         },
+         params: {
+            acc_email: 4
+         }
+      }).then(res => {
+         console.log(res.data);
+         setUpToDate(false);
+      });
+   }
 
    return (
       <div>
@@ -34,28 +73,6 @@ function BackendAvatar() {
          </Form>
       </div>
    );
-}
-
-function uploadAvatar(e) {
-   // 檔案位置
-   // console.log(e.target.files[0]);
-
-   // 新增 formData
-   const formData = new FormData();
-   formData.append('image', e.target.files[0]);
-   formData.append('acc_email', 'ggg@mail.com');
-
-   // 傳送資料
-   axios.put('http://localhost:5000/account/updateavatar', formData,
-      {
-         headers: {
-            'Content-Type': 'multipart/form-data',
-         }
-      }
-   ).then(res => {
-      console.log(res.data);
-   });
-
 }
 
 export default BackendAvatar;
