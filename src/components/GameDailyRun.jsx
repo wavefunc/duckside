0718876015515f5cc9a90274b5bffa6d796e7289
s520334455@gmail.com <s520334455@gmail.com>
2022-03-03 14:21:43 +0800

    2022/03/03 選擇關卡及查詢資料

--- src/components/GameDailyRun.jsx
+++ src/components/GameDailyRun.jsx
@@ -48,27 +48,28 @@ function GameDailyRun() {
 
 
    //抓取股市資料
+   let labels= [ '2018/12/22', '2018/12/24', '2018/12/25', '2018/12/26', '2018/12/27','2018/12/28','2019/01/02'];
    const getPrice = () => {
       const fakedata = {
          labels: [ '2018/12/22', '2018/12/24', '2018/12/25', '2018/12/26', '2018/12/27','2018/12/28','2019/01/02'],
          datasets: [
             {
                type: 'bar',
-               label: 'Dataset 1',
-               data: [100, 200, 30, 400, 500, 60, 700],
-               backgroundColor: 'rgba(255, 99, 132)',
+               label: '買價',
+               data: labels.map(() => Math.floor((3-Math.random())*100)),
+               backgroundColor: 'rgba(255, 30, 32)',
             },
             {
                type: 'line',
-               label: 'Dataset 2',
-               data: [20, 300, 40, 500, 60, 700, 80],
-               backgroundColor: 'rgba(53, 162, 235)',
+               label: '走勢',
+               data: labels.map(() => Math.floor((0.5-Math.random())*300)),
+               backgroundColor: 'rgba(53, 30, 235)',
             },
             {
                type: 'bar',
-               label: 'Dataset 2',
-               data: [-200, -30, -400, -50, -380, -70, -100],
-               backgroundColor: 'rgb(53, 162, 235)',
+               label: '賣價',
+               data: labels.map(() => Math.floor((2-Math.random())*150)),
+               backgroundColor: 'rgb(53, 136, 20)',
             }
          ],
       };
