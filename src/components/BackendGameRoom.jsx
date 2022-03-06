18744bc3fb2f6476542ef0b21e57527093d0214d
wavefunc <wavefunc@gmail.com>
2022-03-06 19:37:33 +0800

    update

--- src/components/BackendGameRoom.jsx
+++ src/components/BackendGameRoom.jsx
@@ -2,13 +2,15 @@ import "../css/GameRoomMain_style.css";
 import Axios from "axios";
 import React, { useState, useEffect } from 'react';
 import Furniture from "./BackendFurniture";
-import Draggable, { DraggableCore } from "react-draggable";
+import Draggable from "react-draggable";
 import { Container } from 'react-bootstrap';
 
 
 function GameRoom() {
    const [pos, setPos] = useState({ x: 0.5, y: 0.5 }); // 設定元件在 Container 內相對比例的x|y位置
    const [container, setContainer] = useState({}); // 用來讀取 Container tag 的 x, y, width, height
+   const [objPos, setObjPos] = useState({ x: 100, y: 100 }); // 用來設定元件的位置
+   var timeout = false;
 
    const eventDrag = (e, data) => {
       setPos({ x: e.x, y: e.y });
@@ -16,29 +18,32 @@ function GameRoom() {
 
    const getContainerPos = (e) => {
 
-      // console.log('x: ', container.x);
-      // console.log('y: ', container.y);
-      // console.log('width: ', container.width);
-      // console.log('height: ', container.height);
    };
 
    const getRelativePos = (posOrigin) => {
-      console.log(posOrigin.x);
+      // console.log(posOrigin.x);
       // (posOrigin.x - container.x)* newcontainer.width / container.width
    };
 
-   var temp = 300;
+   const setPosition = () => {
+      // setContainer(document.getElementById("container").getBoundingClientRect());
+      const { x, y } = objPos;
+      setObjPos({ x: x + 100, y: 100 });
+   };
+
+   // window size 改變時的 event
+   window.addEventListener('resize', () => {
+      // 利用 timeout 避免 resize 連續被觸發
+      clearTimeout(timeout);
+      timeout = setTimeout(setPosition, 250);
+   });
+
+   // setPosition();
+
 
    useEffect(() => {
 
-      // window size 改變時的 event
-      window.addEventListener('resize', () => {
-         setContainer(document.getElementById("container").getBoundingClientRect());
-         // setPos({x: , y:})
-         getRelativePos(pos);
-      });
-
-
+      // console.log(objPos);
    }, []);
 
    return (
@@ -49,12 +54,13 @@ function GameRoom() {
          }}
          onClick={getContainerPos}
       >
-         <label>{JSON.stringify(container)}</label>
+         <label>{JSON.stringify(objPos)}</label>
          <button onClick={getContainerPos}>checkPos</button>
          <Draggable
             onDrag={eventDrag}
-            // defaultPosition={{ x: (0.5 * container.width + container.x), y: (0.5 * container.height + container.y) }}
-            defaultPosition={{ x: container.width , y: 300 }}
+            // Position={{ x: (0.5 * container.width + container.x) || 300, y: (0.5 * container.height + container.y) || 300 }}
+            Position={objPos}
+         // defaultPosition={{ x: 300, y: 300 }}
          // bounds='#container'
          >
 
--- src/components/BackendTest.jsx
+++ src/components/BackendTest.jsx
@@ -1,81 +1,262 @@
-// ----- 冠樺 ----- //
-
+// const {ReactDraggable: Draggable, React, ReactDOM} = window;
+import "../css/GameRoomMain_style.css";
+import Axios from "axios";
 import React, { useState, useEffect } from 'react';
+import Furniture from "./BackendFurniture";
+import Draggable from "react-draggable";
+import { Container } from 'react-bootstrap';
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
+class BackendTest extends React.Component {
+   state = {
+      activeDrags: 0,
+      deltaPosition: {
+         x: 0, y: 0
+      },
+      controlledPosition: {
+         x: -400, y: 200
+      }
+   };
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-   apiKey: process.env.REACT_APP_API_KEY,
-   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-   projectId: process.env.REACT_APP_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_APP_ID,
-   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const auth = getAuth();
-const providerGoogle = new GoogleAuthProvider();
-
-function BackendTest() {
-   const [userResult, setUserResult] = useState('');
-   const [logoutResult, setLogoutResult] = useState('');
-
-   getRedirectResult(auth)
-      .then((result) => {
-         const credential = GoogleAuthProvider.credentialFromResult(result);
-         const token = credential.accessToken;
-         const user = result.user;
-         setUserResult(result.user);
-         setLogoutResult('登入中');
-      }).catch((error) => {
-         const errorCode = error.code;
-         const errorMessage = error.message;
-         const email = error.email;
-         const credential = GoogleAuthProvider.credentialFromError(error);
+   handleDrag = (e, ui) => {
+      const { x, y } = this.state.deltaPosition;
+      this.setState({
+         deltaPosition: {
+            x: x + ui.deltaX,
+            y: y + ui.deltaY,
+         }
       });
+   };
 
+   onStart = () => {
+      this.setState({ activeDrags: ++this.state.activeDrags });
+   };
 
-   useEffect(() => {
-      console.log('env: ', process.env.REACT_APP_TEST);
-   }, []);
+   onStop = () => {
+      this.setState({ activeDrags: --this.state.activeDrags });
+   };
+   onDrop = (e) => {
+      this.setState({ activeDrags: --this.state.activeDrags });
+      if (e.target.classList.contains("drop-target")) {
+         alert("Dropped!");
+         e.target.classList.remove('hovered');
+      }
+   };
+   onDropAreaMouseEnter = (e) => {
+      if (this.state.activeDrags) {
+         e.target.classList.add('hovered');
+      }
+   }
+   onDropAreaMouseLeave = (e) => {
+      e.target.classList.remove('hovered');
+   }
 
-   return (
-      <div>
-         <button onClick={() => {
-            signInWithRedirect(auth, providerGoogle);
-         }}>第三方登入 </button>
-         <p>displayName: {userResult.displayName}</p>
-         <p>email: {userResult.email}</p>
-         <p>emailVerified: {userResult.emailVerified}</p>
-         <p>photoURL: {userResult.photoURL}</p>
-         <p>uid: {userResult.uid}</p>
-         <br />
-         <button onClick={() => {
-            signOut().then(() => {
-               // 登出成功
-               setLogoutResult('登出成功');
-            }).catch((error) => {
-               // 有錯誤
-               setLogoutResult('登出失敗');
-            });
-         }}>登出</button>
-         <p>{logoutResult}</p>
-      </div>
-   );
+   // For controlled component
+   adjustXPos = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const { x, y } = this.state.controlledPosition;
+      this.setState({ controlledPosition: { x: x - 10, y } });
+   };
+
+   adjustYPos = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const { controlledPosition } = this.state;
+      const { x, y } = controlledPosition;
+      this.setState({ controlledPosition: { x, y: y - 10 } });
+   };
+
+   onControlledDrag = (e, position) => {
+      const { x, y } = position;
+      this.setState({ controlledPosition: { x, y } });
+   };
+
+   onControlledDragStop = (e, position) => {
+      this.onControlledDrag(e, position);
+      this.onStop();
+   };
+
+   render() {
+      const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
+      const { deltaPosition, controlledPosition } = this.state;
+      return (
+         <div>
+            <h1>React Draggable</h1>
+            <p>Active DragHandlers: {this.state.activeDrags}</p>
+            <p>
+               <a href="https://github.com/STRML/react-draggable/blob/master/example/example.js">Demo Source</a>
+            </p>
+            <Draggable {...dragHandlers}>
+               <div className="box">I can be dragged anywhere</div>
+            </Draggable>
+            <Draggable axis="x" {...dragHandlers}>
+               <div className="box cursor-x">I can only be dragged horizonally (x axis)</div>
+            </Draggable>
+            <Draggable axis="y" {...dragHandlers}>
+               <div className="box cursor-y">I can only be dragged vertically (y axis)</div>
+            </Draggable>
+            <Draggable onStart={() => false}>
+               <div className="box">I don't want to be dragged</div>
+            </Draggable>
+            <Draggable onDrag={this.handleDrag} {...dragHandlers}>
+               <div className="box">
+                  <div>I track my deltas</div>
+                  <div>x: {deltaPosition.x.toFixed(0)}, y: {deltaPosition.y.toFixed(0)}</div>
+               </div>
+            </Draggable>
+            <Draggable handle="strong" {...dragHandlers}>
+               <div className="box no-cursor">
+                  <strong className="cursor"><div>Drag here</div></strong>
+                  <div>You must click my handle to drag me</div>
+               </div>
+            </Draggable>
+            <Draggable handle="strong">
+               <div className="box no-cursor" style={{ display: 'flex', flexDirection: 'column' }}>
+                  <strong className="cursor"><div>Drag here</div></strong>
+                  <div style={{ overflow: 'scroll' }}>
+                     <div style={{ background: 'yellow', whiteSpace: 'pre-wrap' }}>
+                        I have long scrollable content with a handle
+                        {'\n' + Array(40).fill('x').join('\n')}
+                     </div>
+                  </div>
+               </div>
+            </Draggable>
+            <Draggable cancel="strong" {...dragHandlers}>
+               <div className="box">
+                  <strong className="no-cursor">Can't drag here</strong>
+                  <div>Dragging here works</div>
+               </div>
+            </Draggable>
+            <Draggable grid={[25, 25]} {...dragHandlers}>
+               <div className="box">I snap to a 25 x 25 grid</div>
+            </Draggable>
+            <Draggable grid={[50, 50]} {...dragHandlers}>
+               <div className="box">I snap to a 50 x 50 grid</div>
+            </Draggable>
+            <Draggable bounds={{ top: -100, left: -100, right: 100, bottom: 100 }} {...dragHandlers}>
+               <div className="box">I can only be moved 100px in any direction.</div>
+            </Draggable>
+            <Draggable {...dragHandlers}>
+               <div className="box drop-target" onMouseEnter={this.onDropAreaMouseEnter} onMouseLeave={this.onDropAreaMouseLeave}>I can detect drops from the next box.</div>
+            </Draggable>
+            <Draggable {...dragHandlers} onStop={this.onDrop}>
+               <div className={`box ${this.state.activeDrags ? "no-pointer-events" : ""}`}>I can be dropped onto another box.</div>
+            </Draggable>
+            <div className="box" style={{ height: '500px', width: '500px', position: 'relative', overflow: 'auto', padding: '0' }}>
+               <div style={{ height: '1000px', width: '1000px', padding: '10px' }}>
+                  <Draggable bounds="parent" {...dragHandlers}>
+                     <div className="box">
+                        I can only be moved within my offsetParent.<br /><br />
+                        Both parent padding and child margin work properly.
+                     </div>
+                  </Draggable>
+                  <Draggable bounds="parent" {...dragHandlers}>
+                     <div className="box">
+                        I also can only be moved within my offsetParent.<br /><br />
+                        Both parent padding and child margin work properly.
+                     </div>
+                  </Draggable>
+               </div>
+            </div>
+            <Draggable bounds="body" {...dragHandlers}>
+               <div className="box">
+                  I can only be moved within the confines of the body element.
+               </div>
+            </Draggable>
+            <Draggable {...dragHandlers}>
+               <div className="box" style={{ position: 'absolute', bottom: '100px', right: '100px' }}>
+                  I already have an absolute position.
+               </div>
+            </Draggable>
+            <Draggable {...dragHandlers}>
+               <RemWrapper>
+                  <div className="box rem-position-fix" style={{ position: 'absolute', bottom: '6.25rem', right: '18rem' }}>
+                     I use <span style={{ fontWeight: 700 }}>rem</span> instead of <span style={{ fontWeight: 700 }}>px</span> for my transforms. I also have absolute positioning.
+
+                     <br /><br />
+                     I depend on a CSS hack to avoid double absolute positioning.
+                  </div>
+               </RemWrapper>
+            </Draggable>
+            <Draggable defaultPosition={{ x: 25, y: 25 }} {...dragHandlers}>
+               <div className="box">
+                  {"I have a default position of {x: 25, y: 25}, so I'm slightly offset."}
+               </div>
+            </Draggable>
+            <Draggable positionOffset={{ x: '-10%', y: '-10%' }} {...dragHandlers}>
+               <div className="box">
+                  {'I have a default position based on percents {x: \'-10%\', y: \'-10%\'}, so I\'m slightly offset.'}
+               </div>
+            </Draggable>
+            <Draggable position={controlledPosition} {...dragHandlers} onDrag={this.onControlledDrag}>
+               <div className="box">
+                  My position can be changed programmatically. <br />
+                  I have a drag handler to sync state.
+                  <div>
+                     <a href="#" onClick={this.adjustXPos}>Adjust x ({controlledPosition.x})</a>
+                  </div>
+                  <div>
+                     <a href="#" onClick={this.adjustYPos}>Adjust y ({controlledPosition.y})</a>
+                  </div>
+               </div>
+            </Draggable>
+            <Draggable position={controlledPosition} {...dragHandlers} onStop={this.onControlledDragStop}>
+               <div className="box">
+                  My position can be changed programmatically. <br />
+                  I have a dragStop handler to sync state.
+                  <div>
+                     <a href="#" onClick={this.adjustXPos}>Adjust x ({controlledPosition.x})</a>
+                  </div>
+                  <div>
+                     <a href="#" onClick={this.adjustYPos}>Adjust y ({controlledPosition.y})</a>
+                  </div>
+               </div>
+            </Draggable>
+
+         </div>
+      );
+   }
 }
+
+class RemWrapper extends React.Component {
+   // PropTypes is not available in this environment, but here they are.
+   // static propTypes = {
+   //   style: PropTypes.shape({
+   //     transform: PropTypes.string.isRequired
+   //   }),
+   //   children: PropTypes.node.isRequired,
+   //   remBaseline: PropTypes.number,
+   // }
+
+   translateTransformToRem(transform, remBaseline = 16) {
+      const convertedValues = transform.replace('translate(', '').replace(')', '')
+         .split(',')
+         .map(px => px.replace('px', ''))
+         .map(px => parseInt(px, 10) / remBaseline)
+         .map(x => `${x}rem`)
+      const [x, y] = convertedValues
+
+      return `translate(${x}, ${y})`
+   }
+
+   render() {
+      const { children, remBaseline = 16, style } = this.props
+      const child = React.Children.only(children)
+
+      const editedStyle = {
+         ...child.props.style,
+         ...style,
+         transform: this.translateTransformToRem(style.transform, remBaseline),
+      }
+
+      return React.cloneElement(child, {
+         ...child.props,
+         ...this.props,
+         style: editedStyle
+      })
+   }
+}
+
 
 export default BackendTest;
