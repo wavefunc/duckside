14f3b1c9e985d3551f241bb1b2b699a6852dcd4c
renhz2017 <blestmilk@hotmail.com>
2022-03-10 14:54:17 +0800

    debug 送出按鈕跑版

--- src/pages/ManageAsset.jsx
+++ src/pages/ManageAsset.jsx
@@ -291,7 +291,7 @@ function ManageAsset(props) {
                                  type="text"
                               />
                            </Col>
-                           <Col lg={1} className="d-inline-flex flex-column-reverse input-group p-2">
+                           <Col lg={2} className="d-inline-flex flex-column-reverse input-group p-2">
                               <Button type="submit" variant="warning" size="sm">送出</Button>
                            </Col>
                         </Row>
